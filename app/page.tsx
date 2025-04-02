815d324dad2961b040196baaf492add96eb02e67
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-04-02 15:55:11 +0530

    new project update

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <div className="w-full md:w-1/3 mx-auto">
                 <BlurFade delay={0.25} inView>
                     <header className="mb-10">
-                        <h1 className="text-xl md:text-3xl font-bold mb-2">Hi, I&apos;m Vinod KR</h1>
+                        <h1 className="text-xl md:text-3xl font-bold">Hi, I&apos;m Vinod KR</h1>
                         <p className="text-gray-400 mb-6">I&apos;m a full-stack engineer.</p>
 
                         <div className="flex space-x-4">
@@ -27,13 +27,20 @@ export default function Home() {
 
                     <p className="text-gray-400 mb-8 text-xs ms:text-sm">here are my crafts</p>
 
-                    <div className="space-y-12">
+                    <div className="space-y-8">
+                        <ProjectCard
+                            title="Vin-UI"
+                            status="on-going"
+                            statusColor="blue"
+                            description="personal React components library."
+                            href="/projects/Vin-UI"
+                        />
                         <ProjectCard
                             title="Portfolio"
                             status="maintained"
                             statusColor="green"
                             description="My personal portfolio website showcasing my work."
-                            href="/projects/space"
+                            href="https://portfolio-vinokr.vercel.app"
                         />
                     </div>
                 </BlurFade>
