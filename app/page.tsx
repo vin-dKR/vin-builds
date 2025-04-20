74c98ac95952b3953eaee559011527ab636cc74c
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-04-20 22:17:57 +0530

    vin-ui-cli added

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import { BlurFade } from "@/components/magicui/blur-fade"
 export default function Home() {
     return (
         <main className="min-h-screen bg-black text-white p-6 md:p-12 lg:p-24">
-            <div className="w-full md:w-1/3 mx-auto">
+            <div className="w-full lg:w-1/3 md:w-1/4 mx-auto">
                 <BlurFade delay={0.25} inView>
                     <header className="mb-10">
                         <h1 className="text-xl md:text-3xl font-bold">Hi, I&apos;m Vinod KR</h1>
-                        <p className="text-gray-400 mb-6">I&apos;m a full-stack engineer.</p>
+                        <p className="text-gray-400 mb-6 lg:text-md text-xs">I&apos;m a full-stack engineer.</p>
 
                         <div className="flex space-x-4">
                             <Link target="_blank" href="https://github.com/vin-dKR" className="text-gray-400 hover:text-white">
@@ -28,6 +28,14 @@ export default function Home() {
                     <p className="text-gray-400 mb-8 text-xs ms:text-sm">here are my crafts</p>
 
                     <div className="space-y-8">
+                        <ProjectCard
+                            title="Vin-UI CLI"
+                            status="completed"
+                            statusColor="orange"
+                            description="CLI based on RUST(btw) for next js ui library."
+                            href="https://github.com/vin-dKR/vin-ui-cmd"
+                        />
+
                         <ProjectCard
                             title="Vin-UI"
                             status="on-going"
