4fb9a7dd08f4eef7f7bd64ca2e6a0efb6b31cbe6
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-07-12 16:39:26 +0530

    new: web-3 project

--- constant/web3-projects.ts
+++ constant/web3-projects.ts
@@ -1,4 +1,3 @@
-
 type Project = {
     title: string;
     status: string;
@@ -14,19 +13,17 @@ type Project = {
 
 export const web3Projects: Project[] = [
     {
-        title: "Hellium AI",
+        title: "SOL Faucet",
         status: "completed",
         statusColor: "green",
-        description: "AI powered sales assistant! Embed Hellium Bot",
-        href: "https://hellium.vercel.app/",
+        description: "SOL Airdropper for devnet and testnet",
+        href: "https://sol-faucet-gamma.vercel.app/",
         stack: "Full Stack",
         technologies: [
             { name: "Next.js", icon: "/tech/nextjs.svg" },
             { name: "Node.js", icon: "/tech/nodejs.svg" },
             { name: "Typescript", icon: "/tech/typescript.svg" },
-            { name: "Prisma", icon: "tech/prisma.svg" },
-            { name: "Clerk", icon: "tech/clerk.svg" },
-            { name: "PostgreSQL", icon: "/tech/postgres.svg" }
+            { name: "Solana", icon: "tech/solana.svg" },
         ]
     }
 ]
